3410cf21eb0a5e1a8c30ad7ee807f4360c22ef59
Joshua Koo <zz85nus@gmail.com>
2017-01-13 03:10:06 +0800

    Split objects

--- bomber.js
+++ bomber.js
@@ -19,54 +19,6 @@ const COLUMNS = 11;
 const ROWS = 11;
 const CELL_PIXELS = 40;
 
-class Walls {
-	constructor(columns, rows) {
-		this.columns = columns;
-		this.rows = rows;
-		this.cells = new Array(columns * rows);
-	}
-
-	get(x, y) {
-		return this.cells[this.index(x, y)];
-	}
-
-	index(x, y) {
-		return x + this.columns * y;
-	}
-
-	coords(i) {
-		const x = i % this.columns;
-		const y = i / this.columns | 0;
-		return [x, y];
-	}
-
-	defaultWalls() {
-		for (let i=1; i < this.rows; i+=2) {
-			for (let j=1; j < this.columns; j+=2) {
-				this.cells[this.index(j, i)] = 1;
-			}
-		}
-	}
-
-	forEach(cb) {
-		for (let row = 0; row < this.rows; row++) {
-			for (let col = 0; col < this.columns; col++) {
-				cb(col, row, this.get(col, row));
-			}
-		}
-	}
-
-	debugWalls() {
-		let s = '';
-		for (let row = 0; row < this.rows; row++) {
-			for (let col = 0; col < this.columns; col++) {
-				s += this.get(col, row) ? '#' : '.';
-			}
-			s += '\n'
-		}
-		return s;
-	}
-}
 // Cell Coordinates
 // Pixels Coordinates
 
@@ -96,6 +48,7 @@ const ctx = canvas.getContext('2d');
 var map = new Walls(COLUMNS, ROWS);
 map.defaultWalls();
 
+
 const s = map.debugWalls();
 pre.innerHTML = s;
 console.log(s);
@@ -106,25 +59,6 @@ map.forEach((x, y, v) => {
 	ctx.fillRect(x * CELL_PIXELS, y * CELL_PIXELS, CELL_PIXELS, CELL_PIXELS);
 	ctx.strokeRect(x * CELL_PIXELS, y * CELL_PIXELS, CELL_PIXELS, CELL_PIXELS);
 })
-
-
-var Player = function() {
-	this.positionAt(0,0)
-};
-
-Player.prototype.positionAt = function(column, row ) {
-	positionAt(this.mesh.position, column, row);
-};
-
-Player.prototype.moveBy = function( x, z ) {
-	this.mesh.position.x += x;
-	this.mesh.position.z += z;
-};
-
-function positionAt(vector, column, row) {
-	vector.x = column * cellWidth - map.width/2 + cellWidth/2;
-	vector.z = row * cellHeight - map.height/2 + cellHeight/2;
-}
 
 document.addEventListener( 'keydown', onDocumentKeyDown, false );
 document.addEventListener( 'keyup', onDocumentKeyUp, false );
---
+++ objects.js
@@ -0,0 +1,64 @@
+class Walls {
+	constructor(columns, rows) {
+		this.columns = columns;
+		this.rows = rows;
+		this.cells = new Array(columns * rows);
+	}
+
+	get(x, y) {
+		return this.cells[this.index(x, y)];
+	}
+
+	index(x, y) {
+		return x + this.columns * y;
+	}
+
+	coords(i) {
+		const x = i % this.columns;
+		const y = i / this.columns | 0;
+		return [x, y];
+	}
+
+	defaultWalls() {
+		for (let i=1; i < this.rows; i+=2) {
+			for (let j=1; j < this.columns; j+=2) {
+				this.cells[this.index(j, i)] = 1;
+			}
+		}
+	}
+
+	forEach(cb) {
+		for (let row = 0; row < this.rows; row++) {
+			for (let col = 0; col < this.columns; col++) {
+				cb(col, row, this.get(col, row));
+			}
+		}
+	}
+
+	debugWalls() {
+		let s = '';
+		for (let row = 0; row < this.rows; row++) {
+			for (let col = 0; col < this.columns; col++) {
+				s += this.get(col, row) ? '#' : '.';
+			}
+			s += '\n'
+		}
+		return s;
+	}
+}
+
+class Player {
+	constructor() {
+		this.positionAt(0,0);
+	}
+
+	positionAt(x, y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	moveBy( dx, dy ) {
+		this.x += dx;
+		this.y += dy;
+	};
+}
