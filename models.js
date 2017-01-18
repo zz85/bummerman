28bc4ead0be289375ad2255c5f1ff551a735867d
Joshua Koo <zz85nus@gmail.com>
2017-01-18 14:56:19 +0800

    Move to new models file

---
+++ models.js
@@ -0,0 +1,184 @@
+const UNITS = 10;
+
+function createSoftWall() {
+	const wallGeometry = new THREE.BoxBufferGeometry(8, 8, 8);
+	const wallMaterial = new THREE.MeshToonMaterial({
+		color: new THREE.Color().setRGB(0.98, 0.45, 1),
+		specular: new THREE.Color().setRGB(1, 1, 1),
+		shininess: 0.5,
+		reflectivity: 0.5,
+		shading: THREE.SmoothShading,
+	});
+
+	const wallMesh = new THREE.Mesh(wallGeometry, wallMaterial);
+	wallMesh.position.y = UNITS / 2;
+
+	return wrap(wallMesh); 
+}
+
+function wrap(mesh) {
+	const object = new THREE.Object3D();
+	object.add(mesh); 
+	return object;
+}
+
+
+// Floor
+
+function createFloor() {
+	const floorGeometry = new THREE.PlaneBufferGeometry(UNITS, UNITS, 1, 1);
+	const wallMaterial = new THREE.MeshToonMaterial({
+		color: new THREE.Color().setRGB(0.1, 0.85, 0.1),
+		specular: new THREE.Color().setRGB(1, 1, 1),
+		shininess: 0.5,
+		reflectivity: 0.5,
+		shading: THREE.SmoothShading,
+	});
+
+	const floorMesh = new THREE.Mesh(floorGeometry, wallMaterial);
+	floorMesh.rotation.x = -Math.PI / 2;
+
+	return wrap(floorMesh);
+}
+	// Hard Wall
+function createHardWall() {
+	const wallGeometry = new THREE.BoxBufferGeometry( 8, 8, 8 );
+	const hardwallMaterial = new THREE.MeshToonMaterial( {
+		color: new THREE.Color().setRGB(0.15, 0.15, .15),
+		specular: new THREE.Color().setRGB(1, 1, 1),
+		shininess: 0.0,
+		reflectivity: 0.0
+	} );
+
+	const hardWall = new THREE.Mesh(wallGeometry, hardwallMaterial);
+	hardWall.position.y = UNITS / 2;
+	return wrap(hardWall);
+}
+
+function createBomb() {
+	const bomb = new THREE.Object3D();
+	
+	const bombMaterial = new THREE.MeshToonMaterial({
+		color: new THREE.Color().setRGB(0.15, 0.15, .15),
+		specular: new THREE.Color().setRGB(1, 1, 1),
+		shininess: 0.0,
+		reflectivity: 0.0,
+		// wireframe: true
+	});
+
+	const sphereGeometry = new THREE.SphereBufferGeometry(5.2, 12, 12);
+	const ball = new THREE.Mesh(sphereGeometry, bombMaterial);
+	bomb.add(ball);
+
+	const capGeometry = new THREE.CylinderBufferGeometry( 2.5, 2.5, 1, 12 );
+	const cap = new THREE.Mesh(capGeometry, bombMaterial);
+	cap.position.y = 5;
+	bomb.add(cap);
+
+	const Path = THREE.Curve.create(
+		function () {
+		},
+
+		function ( t ) { //getPoint: t is between 0-1
+			var tx = t; // Math.cos( t ) * 2 * 
+			var ty = t * 3.5;
+			var tz = Math.sin( t ) * 2 * t;
+
+			return new THREE.Vector3( tx, ty, tz );
+		}
+	);
+
+	const path = new Path();
+
+	const fuseGeometry = new THREE.TubeBufferGeometry( path, 20, 0.4, 8, false );
+	const fuse = new THREE.Mesh(fuseGeometry, bombMaterial);
+	fuse.position.y = 5
+
+	bomb.add(fuse);
+
+	bomb.position.y = UNITS / 2;
+	bomb.scale.multiplyScalar(0.5);
+
+	return wrap(bomb);
+}
+
+function createHero() {
+	const man = new THREE.Object3D();
+
+	headMaterial = new THREE.MeshToonMaterial( {
+		color: new THREE.Color().setRGB(0.35, 0.15, .95),
+		specular: new THREE.Color().setRGB(1, 1, 1),
+		shininess: 0.0,
+		reflectivity: 0.0,
+		// wireframe: true
+	} );
+
+	var roundedRectShape = new THREE.Shape();
+	( function roundedRect( ctx, x, y, width, height, radius ){
+
+		ctx.moveTo( x, y + radius );
+		ctx.lineTo( x, y + height - radius );
+		ctx.quadraticCurveTo( x, y + height, x + radius, y + height );
+		ctx.lineTo( x + width - radius, y + height) ;
+		ctx.quadraticCurveTo( x + width, y + height, x + width, y + height - radius );
+		ctx.lineTo( x + width, y + radius );
+		ctx.quadraticCurveTo( x + width, y, x + width - radius, y );
+		ctx.lineTo( x + radius, y );
+		ctx.quadraticCurveTo( x, y, x, y + radius );
+
+	} )( roundedRectShape, -1, -1, 2, 2, 1 );
+
+	var extrudeSettings = { amount: .6, bevelEnabled: true, bevelSegments: 2, steps: 2, bevelSize: .2, bevelThickness: .2 }
+	var headGeometry = new THREE.ExtrudeGeometry( roundedRectShape, extrudeSettings );
+
+	head = new THREE.Mesh(headGeometry, headMaterial);
+
+	tailGeometry = new THREE.SphereBufferGeometry(0.4, 8, 8);
+	tail = new THREE.Mesh(tailGeometry, headMaterial);
+	tail.position.y = 1;
+	tail.position.z = -0.6;
+	head.add(tail);
+
+	var faceGeometry = new THREE.CircleBufferGeometry( 3, 12 );
+
+	head.position.z = -0.2;
+	head.position.y = 1.9;
+	head.scale.y = 0.8;
+	face = new THREE.Mesh(faceGeometry, headMaterial);
+	face.rotation.y = Math.PI;
+	// head.add(face);
+	man.add(head);
+	man.position.y = UNITS / 2;
+
+	// Egg formula from http://www.mathematische-basteleien.de/eggcurves.htm
+	// x^2 + (1.4 ^ x * 1.6 * y)^2 = 1;
+	// (1.4 ^ x * 1.6 * y)^2 = 1 - x^2;
+	// 1.4 ^ x * 1.6 * y = Math.pow(1 - x^2, 0.5);
+	// y = Math.pow(1 - x^2, 0.5) / (1.4 ^ x * 1.6)
+
+	eggFormula = x => Math.pow(1 - x ** 2, 0.5) / (1.4 ** x * 1.6)
+
+	const divisions = 15;
+	const spline = [];
+	for (let x = 0; x <= divisions; x++) {
+		const t = (x / divisions - 0.5) * 2;
+		spline.push(new THREE.Vector2(eggFormula(t), t));
+	}
+
+	bodyGeometry = new THREE.LatheBufferGeometry( spline );
+	var material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
+	var body = new THREE.Mesh( bodyGeometry, headMaterial );
+
+	var armGeometry = new THREE.CylinderBufferGeometry( .1, .14, 1.5, 8 );
+	arm = new THREE.Mesh(armGeometry, headMaterial);
+	arm.position.y = 0.5;
+	// arm.position.x = 1;
+	arm.rotation.z = Math.PI / 2;
+
+	man.add(arm);
+
+	man.add(body);
+	man.scale.multiplyScalar(2);
+
+	return wrap(man);
+}
