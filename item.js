872bd4fa7f12568cfb5f4e56a40d6c45d27a0f6e
Joshua Koo <zz85nus@gmail.com>
2025-12-30 22:42:26 -0800

    Add weighted item spawn probabilities (kick is rarer)

--- item.js
+++ item.js
@@ -1,14 +1,30 @@
 class Item {
+	static TYPES = {
+		SPEED_UP: 0,
+		BOMBS_UP: 1,
+		FIRE_UP: 2,
+		KICK: 3,
+	};
+
+	// Spawn weights (higher = more common)
+	static WEIGHTS = [30, 30, 30, 10];
+
+	static randomType() {
+		const weights = Item.WEIGHTS;
+		const total = weights.reduce((a, b) => a + b, 0);
+		let r = Math.random() * total;
+		for (let i = 0; i < weights.length; i++) {
+			r -= weights[i];
+			if (r <= 0) return i;
+		}
+		return 0;
+	}
+
 	constructor(x, y, type) {
 		this.x = x;
 		this.y = y;
 
-		Object.assign(this, {
-			SPEED_UP: 0,
-			BOMBS_UP: 1,
-			FIRE_UP: 2,
-			KICK: 3,
-		});
+		Object.assign(this, Item.TYPES);
 
 		this.type = type;
 	}
